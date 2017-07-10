bc7770c06e8232b63cd235753f7ad3e62a52565c
Jim Padilla <jp_code2013@yahoo.com>
2017-07-10 13:58:10 -0700

    TypeScript objects

---
+++ typescript-objects.js
@@ -0,0 +1,14 @@
+// Objects in Typescript
+// Sample of data returned from API
+var realUser = {
+    email: 'test@test.com',
+    firstName: 'James',
+    lastName: 'Padilla',
+    // objects can also hold functions
+    sayHi: function () {
+        return "Hello";
+    }
+};
+console.log(realUser.email);
+console.log(realUser.sayHi());
+//# sourceMappingURL=typescript-objects.js.map
---
+++ typescript-objects.ts
@@ -0,0 +1,15 @@
+// Objects in Typescript
+
+  // Sample of an object returned from API
+var realUser = {
+  email: 'test@test.com',
+  firstName: 'James',
+  lastName: 'Padilla',
+  // objects can also hold functions
+  sayHi() {
+    return "Hello";
+  }
+};
+
+// console.log(realUser.email);
+// console.log(realUser.sayHi());
