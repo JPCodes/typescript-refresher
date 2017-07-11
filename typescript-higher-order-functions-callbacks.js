c99c7c6ee9892a589825e78ebd462320c75e5d09
Jim Padilla <jp_code2013@yahoo.com>
2017-07-10 18:00:10 -0700

    Add comments to higher order functions and callbacks file

---
+++ typescript-higher-order-functions-callbacks.js
@@ -0,0 +1,20 @@
+// TypeScript - Higher Order Functions and Callbacks
+// define a callback method
+var dbQuery = function () {
+    setTimeout(function () {
+        console.log('Query results');
+    }, 3000);
+};
+// Higher Order Function - takes a function as an argument
+// queryDB is of type method; declare that void will be returned
+function loadPage(queryDB) {
+    console.log('Header');
+    queryDB(); // run the method, but it's non-blocking to the rest of this function
+    console.log('Sidebar');
+    console.log('Footer');
+}
+// No parenthesis when passed in
+loadPage(dbQuery);
+// vs calling a function normally
+dbQuery();
+//# sourceMappingURL=typescript-higher-order-functions-callbacks.js.map
